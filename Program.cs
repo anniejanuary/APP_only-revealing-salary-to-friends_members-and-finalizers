2500fb0f94a9973d020549ac9ccf55a17ff542f0
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-21 18:20:38 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MembersAndFinalizers_Destructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introduction(true); //"I am a friend, so tell me your salary, Lucy"
+        }
+    }
+}
